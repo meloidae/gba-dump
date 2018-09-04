e50b21725c65c09951b79a0835652e3cb2e4d63a
gyasui <go04jp@yahoo.co.jp>
2018-09-05 03:20:21 +0900

    play around with interrupts

--- gba/source/irq.arm.c
+++ gba/source/irq.arm.c
@@ -1,8 +1,14 @@
 #include <gba_sio.h>
+#include <gba_types.h>
+#include <gba_interrupt.h>
 #include "irq.arm.h"
 #include "dump.h"
 
 void irqSerialHandler() {
+    REG_IME = 0;
+    u16 flag = REG_IF; 
     REG_SIODATA32 = dumpStep();
 	REG_SIOCNT = SIO_IRQ | SIO_32BIT | SIO_START;
+    REG_IF = flag;
+    REG_IME = 1;
 } // irqSerialHandler
