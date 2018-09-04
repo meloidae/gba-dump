38c596b2f719e64b7532d7bfcd42cef91955c155
gyasui <go04jp@yahoo.co.jp>
2018-09-05 04:45:51 +0900

    add a new variable

--- gba/source/main.thumb.c
+++ gba/source/main.thumb.c
@@ -2,12 +2,15 @@
 
 #include "dump.thumb.h"
 #include "irq.arm.h"
+ 
+#define	REG_WAITCNT *(vu16 *)(REG_BASE + 0x204)
 
 int main() {
 	//IrqInit();
+    REG_WAITCNT = 0x4018;
     irqInit();
-    irqEnable(IRQ_VBLANK | IRQ_SERIAL);
     irqSet(IRQ_SERIAL, irqSerialHandler);
+    irqEnable(IRQ_VBLANK | IRQ_SERIAL);
 
     consoleDemoInit();
     dumpInit();
